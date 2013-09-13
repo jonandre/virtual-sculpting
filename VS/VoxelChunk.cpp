8b2d3e1f3990221cfd76c498be1b61573d9a1ff1
Mattias Andrée <maandree@operamail.com>
2013-09-13 19:12:13 +0200

    style and warnings
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- VS/VoxelChunk.cpp
+++ VS/VoxelChunk.cpp
@@ -3,6 +3,7 @@
 #include "VAO.h"
 #include "GraphicsLib.h"
 
+
 //Point* VoxelChunk::_points = NULL;
 //Normal* VoxelChunk::_normals = NULL;
 
@@ -13,16 +14,16 @@ VoxelChunk::VoxelChunk()
 
 void VoxelChunk::CreateGeometry()
 {
-	unsigned int global_size = size*size*size;
+	unsigned int global_size = size * size * size;
 	//Each visual element is point
-	_points = new Point[ global_size ];//currently we are not counting shared points between voxels
-	_colors = new Color[ global_size ];
-	memset( _colors, 0, global_size*sizeof(Color) );
-	_renderable_indexes = new unsigned int [global_size];
+	_points = new Point[global_size]; //currently we are not counting shared points between voxels
+	_colors = new Color[global_size];
+	memset(_colors, 0, global_size * sizeof(Color));
+	_renderable_indexes = new unsigned int[global_size];
 	_vertex_len = global_size;
-	_indexes = new unsigned int [global_size];
-
-	//now we can generate geometry in given chunk - later it can be updated (  color arrays, for example )
+	_indexes = new unsigned int[global_size];
+	
+	//now we can generate geometry in given chunk - later it can be updated(color arrays, for example)
 
 	Color __clr;
 	__clr.comp[0] = 255;
@@ -33,16 +34,16 @@ void VoxelChunk::CreateGeometry()
 	Point pnt;
 	unsigned int index = 0;
 
-	for(int i = 0; i < size; i++)
-    {
-		pnt.coord[0] = float(_lbl[0] + i)+0.5f;
-		for(int j = 0; j < size; j++)
+	for (int i = 0; i != size; i++)
+	{
+		pnt.coord[0] = float(_lbl[0] + i) + 0.5f;
+		for (int j = 0; j != size; j++)
 		{
-			pnt.coord[1] = float(_lbl[1] + j)+0.5f;
-			for( int k = 0; k < size; k++)
+			pnt.coord[1] = float(_lbl[1] + j) + 0.5f;
+			for (int k = 0; k != size; k++)
 			{
-				pnt.coord[2] = float(_lbl[2] + k)+0.5f;
-				index = i*size*size+j*size+k ;
+				pnt.coord[2] = float(_lbl[2] + k) + 0.5f;
+				index = i * size * size + j * size + k;
 				_points[index] = pnt;
 				_colors[index] = __clr;
 			}
@@ -51,15 +52,15 @@ void VoxelChunk::CreateGeometry()
 
 }
 
-VoxelChunk::VoxelChunk( const Point& center, int lbl[3], int ufr[3]  )
+VoxelChunk::VoxelChunk(const Point& center, int lbl[3], int ufr[3])
 {
 	_center = center;
 	_vao = NULL;
 	_vbo = NULL;
 	_dirty = false;
 	size = ufr[0] - lbl[0];
-	memcpy( _lbl, lbl, 3*sizeof(int));
-	memcpy( _ufr, ufr, 3*sizeof(int));
+	memcpy(_lbl, lbl, 3 * sizeof(int));
+	memcpy(_ufr, ufr, 3 * sizeof(int));
 	_points = NULL;
 	_colors = NULL;
 	_vertex_len = NULL;
@@ -73,39 +74,37 @@ VoxelChunk::~VoxelChunk()
 	ClearGeometry();
 	ClearMesh();
 }
+
 /*
 static void polygon(int a, int b, int c , int d, Point* vert, Color* clr, GLfloat vertices[][3])
 {
-  vert[0].coord[0] = vertices[a][0];
-  vert[0].coord[1] = vertices[a][1];
-  vert[0].coord[2] = vertices[a][2];
-  
-  vert[1].coord[0] = vertices[b][0];
-  vert[1].coord[1] = vertices[b][1];
-  vert[1].coord[2] = vertices[b][2];
-
-  vert[2].coord[0] = vertices[c][0];
-  vert[2].coord[1] = vertices[c][1];
-  vert[2].coord[2] = vertices[c][2];
-
-  vert[3].coord[0] = vertices[c][0];
-  vert[3].coord[1] = vertices[c][1];
-  vert[3].coord[2] = vertices[c][2];
-
-  vert[4].coord[0] = vertices[d][0];
-  vert[4].coord[1] = vertices[d][1];
-  vert[4].coord[2] = vertices[d][2];
-
-  vert[5].coord[0] = vertices[a][0];
-  vert[5].coord[1] = vertices[a][1];
-  vert[5].coord[2] = vertices[a][2];
-
-
-  for ( int i = 0; i < 6; i++)
-  {
-	  clr[i] = *clr;
-  }
-
+	vert[0].coord[0] = vertices[a][0];
+	vert[0].coord[1] = vertices[a][1];
+	vert[0].coord[2] = vertices[a][2];
+	
+	vert[1].coord[0] = vertices[b][0];
+	vert[1].coord[1] = vertices[b][1];
+	vert[1].coord[2] = vertices[b][2];
+	
+	vert[2].coord[0] = vertices[c][0];
+	vert[2].coord[1] = vertices[c][1];
+	vert[2].coord[2] = vertices[c][2];
+	
+	vert[3].coord[0] = vertices[c][0];
+	vert[3].coord[1] = vertices[c][1];
+	vert[3].coord[2] = vertices[c][2];
+	
+	vert[4].coord[0] = vertices[d][0];
+	vert[4].coord[1] = vertices[d][1];
+	vert[4].coord[2] = vertices[d][2];
+	
+	vert[5].coord[0] = vertices[a][0];
+	vert[5].coord[1] = vertices[a][1];
+	vert[5].coord[2] = vertices[a][2];
+	
+	
+	for (int i = 0; i < 6; i++)
+		clr[i] = *clr;
 }
 */
 
@@ -114,7 +113,7 @@ void VoxelChunk::ClearMesh()
 	if (_vbo)
 		delete _vbo;
 	_vbo = NULL;
-
+	
 	if (_vao)
 		delete _vao;
 	_vao = NULL;
