976687ba3206350282d5e43210983720ed4a9127
Mattias Andrée <maandree@operamail.com>
2013-09-13 18:44:51 +0200

    cleanup and document main
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- VS/VS.cpp
+++ VS/VS.cpp
@@ -15,75 +15,81 @@
  */
 inline double diffclock(clock_t end, clock_t start) 
 {
-    return (end - start) * 1000 / CLOCKS_PER_SEC;
+	return (end - start) * 1000 / CLOCKS_PER_SEC;
 }
 
 
-int main(int argc, UINT8** argv) 
+/**
+ * Main method
+ * 
+ * @param   argc  The number of command line arguments
+ * @param   argv  Command line arguments
+ * @return        Exit value, zero on success
+ */
+int main(int argc, char** argv) 
 {
-	GLContext* cntx = new GLContext();//Window+render.
-	Input* inp = new Input();//Input system.
-	cntx->SetInput( inp );//Context redirects mouse+keyb to Input
-
+	(void) argc;
+	(void) argv;
+	
+	GLContext* cntx = new GLContext(); //Window+render.
+	Input* inp = new Input(); //Input system.
+	cntx->SetInput(inp); //Context redirects mouse+keyb to Input
+	
 	unsigned int power = 8;
-
-	GridModel* model = new GridModel(power);//power of 2
+	
+	GridModel* model = new GridModel(power); //power of 2
 	unsigned int side = model->GetDimm();
-	inp->SetZoom(-(side*4.0f));
-	inp->SetModel( model );
-
-	KinectTool* tool = new KinectTool( (side*0.75f), (side*0.75f), side*.75f, -(side*.75f));
-
+	inp->SetZoom(-(side * 4.0f));
+	inp->SetModel(model);
+	
+	KinectTool* tool = new KinectTool(side * 0.75f, side * 0.75f, side * 0.75f, -(side * 0.75f));
+	
 	Soundify snd;
 	snd.Play();
-
+	
 	int acted = 0;
 	bool space_pressed = false;
 	while (cntx->alive())
 	{	
 		clock_t start = clock();
 		acted = 0;
-
-		inp->UpdateFrame();//Reset frame variables.
-		cntx->doMessage();//Win message loop
-
-
-		tool->DoToolUpdate();//update tool state - like depthmap
 		
-		if ( inp->IsPressed(' ') )
-		{
-			space_pressed = !space_pressed;
-		}
-
-		if ( space_pressed )
-			acted = tool->InteractModel( model, inp->GetObjectQ());//obvious
+		inp->UpdateFrame(); //Reset frame variables.
+		cntx->doMessage(); //Win message loop
 		
-		model->UpdateGrid();// update visual representation of model
-		cntx->renderScene(model, tool, inp->GetViewM(), inp->GetObjectM());// do actual rendering.
+		
+		tool->DoToolUpdate(); //Update tool state - like depthmap
+		
+		if (inp->IsPressed(' '))
+			space_pressed ^= true;
+		
+		if (space_pressed)
+			acted = tool->InteractModel(model, inp->GetObjectQ()); //obvious
+		
+		model->UpdateGrid(); //Update visual representation of model
+		cntx->renderScene(model, tool, inp->GetViewM(), inp->GetObjectM()); //Do actual rendering.
 		
 		///
 		//static int tmp = 0;
 		//tmp++;
-		//acted = (rand()%100) > 10 ? (rand()%3000) : 0;
+		//acted = (rand() % 100) > 10 ? (rand() % 3000) : 0;
 		//acted = tmp;
 		///
-
-		if (acted)//sonification
+		
+		if (acted) //sonification
 		{
 			snd.SetGain(1.0f);
-			snd.SetPitch( 0.1 + glm::log2(acted*1.0f)/1000.0f );
+			snd.SetPitch(0.1 + glm::log2(acted * 1.0f)/1000.0f);
 		}
 		else
-		{
 			snd.SetGain(0.0f);
-		}
-
+		
 		clock_t end = clock();
-		//std::cout<<"Frame time = " << diffclock( end, start )<< " ms, " << "Interacted: "<< acted << std::endl;
+		//std::cout << "Frame time = " << diffclock(end, start) << " ms, " << " Interacted: " << acted << std::endl;
 	}
-
+	
 	snd.SetGain(0.0f);
-
+	
 	delete model;
 	delete inp;
 	delete tool;
