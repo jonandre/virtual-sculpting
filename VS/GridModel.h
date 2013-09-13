7ee423fc42c49e223c94bfa3075b2fdddc46904a
Mattias Andrée <maandree@operamail.com>
2013-09-13 19:51:02 +0200

    style + warnings
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- VS/GridModel.h
+++ VS/GridModel.h
@@ -9,18 +9,19 @@ class GridModel: public Model
 {
 public:
 	GridModel();
-	GridModel( int power );
-
+	GridModel(int power);
+	
 	~GridModel();
-	int UpdateCellMelt( int i, int j, int k, UINT8 val );
-	int UpdateCellAdd( int i, int j, int k, UINT8 val );
+	int UpdateCellMelt(int i, int j, int k, UINT8 val);
+	int UpdateCellAdd(int i, int j, int k, UINT8 val);
 	UINT8* GetCells();
 	unsigned int GetSize();
 	unsigned int GetDimm();
 	void UpdateGrid();
-	inline  unsigned int GetCellIndex( const Point& pos, unsigned int &x, unsigned int &y, unsigned int &z );
-	std::map< unsigned int, VAO* >* GetRenderableCells();
-	void ReInitModel( bool clear );
+	inline unsigned int GetCellIndex(const Point& pos, unsigned int &x, unsigned int &y, unsigned int &z);
+	std::map<unsigned int, VAO*>* GetRenderableCells();
+	void ReInitModel(bool clear);
+	
 private:
 	//GridCell* _cells;
 	unsigned int dimm;
@@ -28,16 +29,16 @@ private:
 	unsigned int size;
 	void EnshureMarked(int i, int j, int k);
 	
-	//std::map< unsigned int, RenderableCell* > _renderable_cells;
-	//std::vector< GridCell* > _dirty_cells;
-	inline bool EvaluateCell( unsigned int x, unsigned int y, unsigned int z );
-
+	//std::map<unsigned int, RenderableCell*> _renderable_cells;
+	//std::vector<GridCell*> _dirty_cells;
+	inline bool EvaluateCell(unsigned int x, unsigned int y, unsigned int z);
+	
 	UINT8* _cells;
 	bool* _interacted;
 	VoxelChunk** _chunks;
-	std::vector< VoxelChunk* > _dirty_chunks;
-	std::vector< VoxelChunk* > _modified_chunks;
-	std::map< unsigned int, VAO* > _renderable_chunks;
+	std::vector<VoxelChunk*> _dirty_chunks;
+	std::vector<VoxelChunk*> _modified_chunks;
+	std::map<unsigned int, VAO*> _renderable_chunks;
 	unsigned int chunk_dimm;
 	unsigned int chunk_size;
 	unsigned int internal_chunk_size;
